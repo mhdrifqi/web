15411b5953e1b13d2adb655d66beb06006ea6ad2
mhdrifqi <130071422+mhdrifqi@users.noreply.github.com>
2023-04-07 00:50:22 +0700

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { logos, socialMediaUrl } from "../Details";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { linkdein, github, twitter } = socialMediaUrl;
+  const toggleClass = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="container mx-auto md:flex justify-between py-2 max-width">
+      <div className="flex justify-between items-center py-2 md:py-10">
+        <NavLink to="/">
+          <img className="w-14" src={logos.logogradient} alt="logo" />
+        </NavLink>
+        <div onClick={toggleClass} className="cursor-pointer">
+          <svg
+            className="stroke-dark-heading dark:stroke-white md:hidden"
+            width="25"
+            height="20"
+            viewBox="0 0 16 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.4375 1.3125H14.5625M1.4375 11.3125H14.5625H1.4375ZM1.4375 6.3125H14.5625H1.4375Z"
+              strokeWidth="1.875"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+      <nav className={` ${!isOpen ? "hidden" : null} text-center md:flex justify-between`}>
+        <ul className="dark:text-light-content font-medium md:flex items-center md:space-x-5 md:mr-10">
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/" onClick={toggleClass}>
+              Home
+            </NavLink>
+          </li>
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/about" onClick={toggleClass}>
+              About
+            </NavLink>
+          </li>
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/technologies" onClick={toggleClass}>
+              Skill
+            </NavLink>
+          </li>
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/projects" onClick={toggleClass}>
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={toggleClass}>
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="flex justify-evenly items-center my-5 md:my-0 md:space-x-5 md:mr-5">
+          <li>
+            <a href={linkdein} target="_blank" rel="noreferrer noopener">
+              <svg
+                className="dark:fill-light-heading fill-dark-heading"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M15 0.599976C7.04701 0.599976 0.600006 7.04698 0.600006 15C0.600006 22.953 7.04701 29.4 15 29.4C22.953 29.4 29.4 22.953 29.4 15C29.4 7.04698 22.953 0.599976 15 0.599976ZM11.475 20.9685H8.55901V11.5845H11.475V20.9685ZM9.99901 10.4325C9.07801 10.4325 8.48251 9.77997 8.48251 8.97297C8.48251 8.14948 9.09601 7.51648 10.0365 7.51648C10.977 7.51648 11.553 8.14948 11.571 8.97297C11.571 9.77997 10.977 10.4325 9.99901 10.4325ZM22.125 20.9685H19.209V15.768C19.209 14.5575 18.786 13.7355 17.7315 13.7355C16.926 13.7355 16.4475 14.292 16.236 14.8275C16.158 15.018 16.1385 15.288 16.1385 15.5565V20.967H13.221V14.577C13.221 13.4055 13.1835 12.426 13.1445 11.583H15.678L15.8115 12.8865H15.87C16.254 12.2745 17.1945 11.3715 18.768 11.3715C20.6865 11.3715 22.125 12.657 22.125 15.42V20.9685Z" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Details.js
@@ -0,0 +1,172 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/Eki 1.png";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/Rif1.png";
+// Tech stack images
+import html from "./assets/techstack/think.png";
+import css from "./assets/techstack/cm.png";
+import sass from "./assets/techstack/tm.png";
+import js from "./assets/techstack/ma.png";
+import react from "./assets/techstack/pm.png";
+import redux from "./assets/techstack/tp.png";
+import tailwind from "./assets/techstack/cw.png";
+import bootstrap from "./assets/techstack/lead.png";
+import vscode from "./assets/techstack/ps.png";
+import github from "./assets/techstack/ai.png";
+import git from "./assets/techstack/pr.png";
+import npm from "./assets/techstack/mo.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/mo.png";
+// Porject Images
+import projectImage1 from "./assets/projects/Social Media Management.jpg";
+import projectImage2 from "./assets/projects/Graphic Design.jpg";
+import projectImage3 from "./assets/projects/Edutainment.jpg";
+import projectImage4 from "./assets/projects/Copy of 3.jpg";
+import projectImage5 from "./assets/projects/w.jpg";
+import projectImage6 from "./assets/projects/Modeling.jpg";
+import projectImage7 from "./assets/projects/Product Photography.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Muhammad Rifqi",
+  tagline: "I am a Graphic Designer | Social Media Enthusiast | Content Marketer",
+  img: profile,
+  about: `Muhammad Rifqi is a Bachelor of Political Science from Universitas Muhammadiyah Yogyakarta and currently working as a Creative Marketer at Carbon is Lyfe. A self-motivated person experienced in project management, content management, and content marketing. Rifqi is also a detail-oriented and hard working person who is exhilarated to learn new things and easy to adapt in a new environment.
+  `,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/muhammad-rifqi-8023a81ab/",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Creative Marketing Staff",
+    Company: `Carbon is Lyfe`,
+    Location: "Banguntapan, DI Yogyakarta",
+    Type: "Full Time",
+    Duration: "Oct 2022 - Present",
+  },
+  {
+    Position: "Social Media Officer",
+    Company: `el.id`,
+    Location: "Sewon, DI Yogyakarta",
+    Type: "Freelance",
+    Duration: "Apr 2022 - Sep 2022",
+  },
+  {
+    Position: "English Content Writer",
+    Company: `Rames Creative`,
+    Location: "Semarang, Jawa Tengah",
+    Type: "Freelance",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Political Science",
+    Company: `Universitas Muhammadiyah Yogyakarta`,
+    Location: "Kasihan, DI Yogyakarta",
+    Type: "Full Time",
+    Duration: "Sep 2017 - May 2021",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Social Media Management",
+    image: projectImage1,
+    description: `I manage social media belonging to fashion brands, starting from content planning, researching, content writing, making briefs, executing designs for Instagram feeds and advertising, making captions, managing KOL, etc.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://drive.google.com/drive/u/4/folders/12ApB2Ai7P9ZEM5P3RYaMEP48NuDEGTSm",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Graphic Design",
+    image: projectImage2,
+    description: `I have several experiences on making graphic design using Adobe Illustrator and Adobe Photoshop, where I define the requirements, visualizing, and creating graphics for Carbon is Lyfe, el.id, boxe, and Jajan Kiw!`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Edutainment Graphic",
+    image: projectImage3,
+    description: `As a Content Marketer, I am responsible for creating written content (carousel) on Instagram starting from conducting research, creating briefs, and visualizing it to increase the engagement of Instagram accounts.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Basic Photography and Videography",
+    image: projectImage4,
+    description: `During my tenure at Carbon is Lyfe and el.id, I am responsible for capturing images for content and advertising purposes.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Video Editing",
+    image: projectImage5,
+    description: `Besides taking videos for content and advertising purposes, I am also responsible for editing videos for Instagram Reels, Tiktok, and Youtube videos.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Modeling",
+    image: projectImage6,
+    description: `I also do modeling for Men's Fashion Brand (Carbon is Lyfe) to display and advertise the products.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Product Photography",
+    image: projectImage7,
+    description: `In my two years experience, I also learned product photography to capture images for catalogs that represent the brand's image.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "rifqi098@gmail.com",
+  phone: "+62895416160190",
+};
